b11c71cc82040891f83f822e373909872d2ceb9c
Murilo Pereira <murilosantos2693@gmail.com>
2016-03-10 00:11:42 -0300

    Criação do core

--- src/App/Core/App.php
+++ src/App/Core/App.php
@@ -4,12 +4,44 @@ namespace App\Core;
 
 class App
 {
-	protected $controler = 'Index';
+	protected $controller = 'Index';
 	protected $action;
 	protected $params = [];
 
-	public function_construct()
+	public function __construct()
 	{
-		
+		$url = $this->parseUrl();
+		$controller = ucfirst($url[0]);
+
+		$className = 'App\\Controller\\' . $this->controller;
+
+		if (class_exists($className)) {
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
+
+		$class = 'App\\Controller\\' . $this->controller;
+		$this->controller = new $class();
+
+		if (isset($url[1])) {
+			$url[1] = $url[1] . 'Action';
+			if (method_exists($this->controller, $url[1])) {
+				$this->action = $url[1];
+				unset($url[1]);
+			}
+			$this->params = array_values($url);
+
+			call_user_func_array([$this->controller, $this->action], $this->params);
+		}
+	}
+
+	public function parseUrl()
+	{
+		$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		$urlArray = explode('/', filter_var(rtrim($url, '/'), FILTER_SANITIZE_URL));
+
+		unset($urlArray[0]);
+
+		return array_values($urlArray)
 	}
 }
